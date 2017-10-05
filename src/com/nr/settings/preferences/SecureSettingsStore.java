408c6ac123d83d6f5f8b2f6726befc539bb67011
SpiritCroc <spiritcroc@gmail.com>
2017-10-05 16:04:07 +0200

    Add SystemSettingsMasterSwitchPreference
    - Import MasterSwitchPreference from Settings(Lib)
    - Add System setting variant + fix initial values
    - Use icon_frame as ic in layout instead of icon_container to fix hiding
      icon
    - Allow launching fragments with the android:fragment extra same as
      currently possible for PreferenceScreens
    - Add possibility to reload value when it might have changed
      (best to call onResume when the value can also be changed from the
      sub preference screen)
    
    Original commit history for TwoTargetPreference from
    frameworks/base/packages/SettingsLib:
    
    commit 8a01abf592762c75bb92a9e1787227c44f1f4a8e
    Author: Fan Zhang <zhfan@google.com>
    
        Adjust icon width to match the width with framework
    
    commit 035ff93430b71ef856f299c9f676f8d340403e16
    Author: Fan Zhang <zhfan@google.com>
    
        Move two target preference layout from settings to lib
    
    Original commit history for MasterSwitchPreference.java:
    
    commit e2ad415fdba79b27dfd2c98a2b01d56db9257c71
    Author: Doris Ling <dling@google.com>
    
        Add content description to the master switch toggle button.
    
    commit c68ae0b9fdd5f5a149364d0af7e8a9497a91adfd
    Author: Julia Reynolds <juliacr@google.com>
    
        Channel settings updates
    
    commit aa805e70ed0c7f4eaa66b6e6a0b79bf1b3b9d16d
    Author: Doris Ling <dling@google.com>
    
        Allow settings preference title to span over multiple lines.
    
    commit fe292515d88e803057a5b6077f67ce284b595d7a
    Author: Fan Zhang <zhfan@google.com>
    
        Refactor the layout for MasterSwitch to a 2 target pref
    
    commit 6467d2027e8fefd646af03c08d51d91386843463
    Author: Doris Ling <dling@google.com>
    
        Extends the touch area for the switch in Master Switch preference.
    
    commit 762a733afa26b3d49edfae81536c9fcbe9384483
    Author: Julia Reynolds <juliacr@google.com>
    
        Allow switch preferences to span lines.
    
    commit 56199e94876d4bbd89fb0272156702a321dd681e
    Author: Fan Zhang <zhfan@google.com>
    
        Misc fix on network & internet screen for developer preview
    
    commit c4c9f4d50eec659eb50f8ed671c27d8c6ef0c924
    Author: Doris Ling <dling@google.com>
    
        Update Network & internet->Wi-Fi to use MasterSwitchPreference.
    
    commit 1432cb852972daedd6c2fde78abb769b722fc47d
    Author: Doris Ling <dling@google.com>
    
        Add new preference class MasterSwitchPreference.
    
    Original commit history for preference_widget_master_switch.xml:
    
    commit fe292515d88e803057a5b6077f67ce284b595d7a
    Author: Fan Zhang <zhfan@google.com>
    
        Refactor the layout for MasterSwitch to a 2 target pref
    
    commit 6467d2027e8fefd646af03c08d51d91386843463
    Author: Doris Ling <dling@google.com>
    
        Extends the touch area for the switch in Master Switch preference.
    
    commit 14c2050330b36a00b708049a691d4e1c39700586
    Merge: 249077a0cd 56199e9487
    Author: TreeHugger Robot <treehugger-gerrit@google.com>
    
        Merge "Misc fix on network & internet screen for developer preview"
    
    commit 56199e94876d4bbd89fb0272156702a321dd681e
    Author: Fan Zhang <zhfan@google.com>
    
        Misc fix on network & internet screen for developer preview
    
    commit 4f1eca05be15b9a30757434ce6c11abe5e6db381
    Author: Doris Ling <dling@google.com>
    
        Update the color for the master switch divider.
    
    commit 1432cb852972daedd6c2fde78abb769b722fc47d
    Author: Doris Ling <dling@google.com>
    
        Add new preference class MasterSwitchPreference.
    
    Change-Id: I7ee76f4626223ec3c66ae7d10ebb87c88c5a8175
    
    Conflicts:
    	src/com/nr/settings/SettingsActivity.java
    
    Conflicts:
    	src/com/nr/settings/preferences/SecureSettingsStore.java

--- src/com/nr/settings/preferences/SecureSettingsStore.java
+++ src/com/nr/settings/preferences/SecureSettingsStore.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.nr.settings.preference;
+package com.nr.settings.preferences;
 
 import android.content.ContentResolver;
 import android.preference.PreferenceDataStore;
