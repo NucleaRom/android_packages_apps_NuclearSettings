AICP/packages_apps_AicpExtras

AE: Add SlimRecents [3/3]
Change-Id: I554e8c9b2644db9c6d86cc8af4bdacac5ce35f98

src/com/aicp/extras/fragments/SlimRecentAppSidebar.java
@@ -1,0 +1,32 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.aicp.extras.fragments;
+
+import android.support.v7.preference.Preference;
+
+import com.aicp.extras.BaseSettingsFragment;
+import com.aicp.extras.R;
+
+public class SlimRecentAppSidebar extends BaseSettingsFragment {
+
+    @Override
+    protected int getPreferenceResource() {
+        return R.xml.slim_recent_app_sidebar;
+    }
+
+}

src/com/aicp/extras/fragments/SlimRecentAppSidebarStyle.java
@@ -1,0 +1,32 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.aicp.extras.fragments;
+
+import android.support.v7.preference.Preference;
+
+import com.aicp.extras.BaseSettingsFragment;
+import com.aicp.extras.R;
+
+public class SlimRecentAppSidebarStyle extends BaseSettingsFragment {
+
+    @Override
+    protected int getPreferenceResource() {
+        return R.xml.slim_recent_app_sidebar_style;
+    }
+
+}

src/com/aicp/extras/fragments/SlimRecents.java
@@ -1,0 +1,94 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.aicp.extras.fragments;
+
+import android.support.v7.preference.Preference;
+import android.support.v14.preference.SwitchPreference;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.Gravity;
+
+import com.aicp.extras.BaseSettingsFragment;
+import com.aicp.extras.R;
+import com.aicp.extras.preference.MasterSwitchPreference;
+
+public class SlimRecents extends BaseSettingsFragment
+        implements Preference.OnPreferenceChangeListener {
+
+    private static final String RECENT_PANEL_LEFTY_MODE = "recent_panel_lefty_mode";
+    private static final String RECENT_PANEL_SHOW_TOPMOST = "recent_panel_show_topmost";
+
+    private SwitchPreference mRecentPanelLeftyMode;
+    private SwitchPreference mShowTopmost;
+    private MasterSwitchPreference mAppSidebar;
+
+    @Override
+    protected int getPreferenceResource() {
+        return R.xml.slim_recents;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mRecentPanelLeftyMode = (SwitchPreference) findPreference(RECENT_PANEL_LEFTY_MODE);
+        mRecentPanelLeftyMode.setOnPreferenceChangeListener(this);
+        mShowTopmost = (SwitchPreference) findPreference(RECENT_PANEL_SHOW_TOPMOST);
+        mAppSidebar =
+                (MasterSwitchPreference) findPreference(Settings.System.USE_RECENT_APP_SIDEBAR);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        boolean recentLeftyMode = Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.RECENT_PANEL_GRAVITY, Gravity.RIGHT) == Gravity.LEFT;
+        mRecentPanelLeftyMode.setChecked(recentLeftyMode);
+
+        mShowTopmost.setEnabled(isMasterDependencyEnabled() && !screenPinningEnabled());
+
+        mAppSidebar.reloadValue();
+    }
+
+    @Override
+    public void setMasterDependencyState(boolean enabled) {
+        super.setMasterDependencyState(enabled);
+        if (mShowTopmost != null) {
+            mShowTopmost.setEnabled(enabled && !screenPinningEnabled());
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mRecentPanelLeftyMode) {
+            Settings.System.putInt(getContext().getContentResolver(),
+                    Settings.System.RECENT_PANEL_GRAVITY,
+                    ((Boolean) newValue) ? Gravity.LEFT : Gravity.RIGHT);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean screenPinningEnabled() {
+        return Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.LOCK_TO_APP_ENABLED, 0) != 0;
+    }
+
+}

NucleaRom/android_packages_apps_NuclearSettings

Conflicts:
	res/values/arrays.xml
	res/values/strings.xml
	res/xml/recents.xml
	src/com/aicp/extras/fragments/Recents.java
	src/com/nr/settings/BaseSettingsFragment.java
	res/values/nr_arrays.xml
	res/values/nr_strings.xml

src/com/nr/settings/fragments/SlimRecentAppSidebar.java
@@ -1,0 +1,32 @@
+/*
+ * Copyright (C) 2017 NR
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.nr.settings.fragments;
+
+import android.support.v7.preference.Preference;
+
+import com.nr.settings.BaseSettingsFragment;
+import com.nr.settings.R;
+
+public class SlimRecentAppSidebar extends BaseSettingsFragment {
+
+    @Override
+    protected int getPreferenceResource() {
+        return R.xml.slim_recent_app_sidebar;
+    }
+
+}

src/com/nr/settings/fragments/SlimRecentAppSidebarStyle.java
@@ -1,0 +1,32 @@
+/*
+ * Copyright (C) 2017 NR
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.nr.settings.fragments;
+
+import android.support.v7.preference.Preference;
+
+import com.nr.settings.BaseSettingsFragment;
+import com.nr.settings.R;
+
+public class SlimRecentAppSidebarStyle extends BaseSettingsFragment {
+
+    @Override
+    protected int getPreferenceResource() {
+        return R.xml.slim_recent_app_sidebar_style;
+    }
+
+}

src/com/nr/settings/fragments/SlimRecents.java
@@ -1,0 +1,94 @@
+/*
+ * Copyright (C) 2017 NR
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.nr.settings.fragments;
+
+import android.support.v7.preference.Preference;
+import android.support.v14.preference.SwitchPreference;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.Gravity;
+
+import com.nr.settings.BaseSettingsFragment;
+import com.nr.settings.R;
+import com.nr.settings.preferences.MasterSwitchPreference;
+
+public class SlimRecents extends BaseSettingsFragment
+        implements Preference.OnPreferenceChangeListener {
+
+    private static final String RECENT_PANEL_LEFTY_MODE = "recent_panel_lefty_mode";
+    private static final String RECENT_PANEL_SHOW_TOPMOST = "recent_panel_show_topmost";
+
+    private SwitchPreference mRecentPanelLeftyMode;
+    private SwitchPreference mShowTopmost;
+    private MasterSwitchPreference mAppSidebar;
+
+    @Override
+    protected int getPreferenceResource() {
+        return R.xml.slim_recents;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mRecentPanelLeftyMode = (SwitchPreference) findPreference(RECENT_PANEL_LEFTY_MODE);
+        mRecentPanelLeftyMode.setOnPreferenceChangeListener(this);
+        mShowTopmost = (SwitchPreference) findPreference(RECENT_PANEL_SHOW_TOPMOST);
+        mAppSidebar =
+                (MasterSwitchPreference) findPreference(Settings.System.USE_RECENT_APP_SIDEBAR);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        boolean recentLeftyMode = Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.RECENT_PANEL_GRAVITY, Gravity.RIGHT) == Gravity.LEFT;
+        mRecentPanelLeftyMode.setChecked(recentLeftyMode);
+
+        mShowTopmost.setEnabled(isMasterDependencyEnabled() && !screenPinningEnabled());
+
+        mAppSidebar.reloadValue();
+    }
+
+    @Override
+    public void setMasterDependencyState(boolean enabled) {
+        super.setMasterDependencyState(enabled);
+        if (mShowTopmost != null) {
+            mShowTopmost.setEnabled(enabled && !screenPinningEnabled());
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mRecentPanelLeftyMode) {
+            Settings.System.putInt(getContext().getContentResolver(),
+                    Settings.System.RECENT_PANEL_GRAVITY,
+                    ((Boolean) newValue) ? Gravity.LEFT : Gravity.RIGHT);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean screenPinningEnabled() {
+        return Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.LOCK_TO_APP_ENABLED, 0) != 0;
+    }
+
+}